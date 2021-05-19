bff2eb5cd0bd05c02865c301f2c0209bb9fc2b1e
Saurabh Kumar <43825814+saurabhkumar23@users.noreply.github.com>
2021-05-19 13:45:36 +0530

    ask for platform

--- main.js
+++ main.js
@@ -16,3 +16,10 @@ let src;
 let dest;
 let contestPath;
 let codeTemplatePath;
+
+// ask for platform
+platform = readline.question('Choose your website:\n1. Codeforces \n2. Leetcode \n3. Codechef \n4. atCoder\n')
+if(platform !=  '1' && platform != '2' && platform != '3' && platform != '4'){
+    console.log('Sorry, we have only these platforms available. choose correct option :)')
+    process.exit()
+}
